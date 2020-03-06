a835243eb4fbbded55c6a23b5cf64fd895926df7
myLovelyWeb <mylovelyweb@mail.ru>
2020-03-07 01:19:46 +0300

    first commit

---
+++ smartgrid.js
@@ -0,0 +1,25 @@
+module.exports = {
+  outputStyle: 'scss',
+  columns: 12,
+  offset: "0px",
+  mobileFirst: true,
+  container: {
+    maxWidth: "1250px",
+    fields: "100px"
+  },
+  breakPoints: {
+    lg: {
+      width: "1250px",
+      fields: "100px"
+    },
+    md: {
+      width: "768px",
+      fields: "50px"
+    },
+    sm: {
+      width: "320px",
+      fields: "30px"
+    }
+  },
+  tab:'  '
+};
