b7d6bb4dd49c19ed38ac179f4a2d3ddaf19bec33
myLovelyWeb <mylovelyweb@mail.ru>
2020-04-07 20:31:22 +0300

    rewrited slider, updated README

---
+++ docs/js/slider.js
@@ -0,0 +1,12 @@
+"use strict";
+
+$(document).ready(function(){
+  $('.reviews__slider').slick({
+    arrows: true,
+    dots: false,
+    accessibility: true,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    mobileFirst: true
+  });
+});
